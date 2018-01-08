dbadc84f2e40d536d54af4efb2063e9d8bd0bd2c
tweiseng <t.weiseng@gmail.com>
2018-01-08 16:13:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = str(name)+ "likes to eat yongtaufoo"
+	new_name = str(name) +"likes to eat yongtaufoo"
 	return render_template('index.html', name=new_name)
 
 
